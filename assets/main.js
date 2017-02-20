788e198a119b1f02a48f54c2ea4bdc14bcb7482d
Mingjie Jiang <jiang@mingjie.info>
2017-02-20 09:03:09 -0500

    Changed meeting time at Feb. 20, 2017

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,8 @@
-  $(document).ready(function(){
-    $('.tooltipped').tooltip({delay: 50});
-     $(".button-collapse").sideNav();
-  });
+$(document).ready(function () {
+  $('.tooltipped').tooltip({ delay: 50 });
+  $(".button-collapse").sideNav();
+  Materialize.toast('New meeting plans available!', 4000)
+});
 
 var host = "mocogeeks.com";
 if (window.location.host == host && window.location.protocol != "https:") {
