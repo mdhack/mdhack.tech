2e73c79770b7fece5e43914d697c4fc547282818
Mingjie Jiang <jiang@mingjie.info>
2017-02-22 18:53:00 -0500

    Removed annoying toast | Removed enforce https

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
   $('.tooltipped').tooltip({ delay: 50 });
   $(".button-collapse").sideNav();
-  Materialize.toast('New meeting plans available!', 4000)
 });
-
-var host = "mocogeeks.com";
-if (window.location.host == host && window.location.protocol != "https:") {
-  window.location.protocol = "https:";
-}
