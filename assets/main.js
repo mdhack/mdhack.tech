060744232122b973b89f72c7b5843385119e3104
Mingjie Jiang <jiang@mingjie.info>
2017-03-09 17:15:22 -0500

    Integrated HackAPI api.mocogeeks.com

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,45 @@
+
+var apiURL = "https://api.mocogeeks.com/apis/meetTime.json";
+
 $(document).ready(function () {
   $('.tooltipped').tooltip({ delay: 50 });
   $(".button-collapse").sideNav();
+  hackApi(apiURL, function (error, data) {
+    if (error) {
+      document.getElementById("api-fetch").innerHTML = "Failed to fetch next meeting data.";
+      console.log('Error found while trying to fetch HackAPI.', error);
+    } else {
+      if (Object.keys(data.beta).length != 0) {
+        document.getElementById("api-time-beta").innerHTML = data.beta.time;
+        document.getElementById("api-date-beta").innerHTML = data.beta.date;
+        document.getElementById("api-loc-beta").innerHTML = data.beta.location;
+        document.getElementById("api-des-beta").innerHTML = data.beta.detail;
+      }
+      else {
+        document.getElementById("api-fetch-beta").innerHTML = "No meeting coming up for Beta Program!"
+      }
+      if (Object.keys(data.wootton).length != 0) {
+        document.getElementById("api-time-wootton").innerHTML = data.wootton.time;
+        document.getElementById("api-date-wootton").innerHTML = data.wootton.date;
+        document.getElementById("api-loc-wootton").innerHTML = data.wootton.location;
+        document.getElementById("api-des-wootton").innerHTML = data.wootton.detail;
+      }
+      else {
+        document.getElementById("api-fetch-wootton").innerHTML = "No meeting coming up for Wootton!"
+      }
+      if (Object.keys(data.blair).length != 0) {
+        document.getElementById("api-time-blair").innerHTML = data.blair.time;
+        document.getElementById("api-date-blair").innerHTML = data.blair.date;
+        document.getElementById("api-loc-blair").innerHTML = data.blair.location;
+        document.getElementById("api-des-blair").innerHTML = data.blair.detail;
+      }
+      else {
+        document.getElementById("api-fetch-blair").innerHTML = "No meeting coming up for Blair!"
+      }
+      document.getElementById("api-fetch-beta").style.display = "";
+      document.getElementById("api-fetch-wootton").style.display = "";
+      document.getElementById("api-fetch-blair").style.display = "";
+    }
+  });
 });
+
